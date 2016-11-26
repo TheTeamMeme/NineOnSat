12a0a95975ce0e8e6cf0294a5b059e8d6005fe6f
MeMyselfandMilo <MeMyselfandMilo@users.noreply.github.com>
2016-11-26 21:35:25 +1100

    Update Time.js

--- Time.js
+++ Time.js
@@ -1,4 +1,4 @@
-function time(id)
+function day(id)
 {
         date = new Date;
         year = date.getFullYear();
@@ -22,7 +22,7 @@ function time(id)
         {
                 s = "0"+s;
         }
-        result = h+':'+m;
+        result = days[day];
         document.getElementById(id).innerHTML = result;
         setTimeout('date_time("'+id+'");','1000');
         return true;
